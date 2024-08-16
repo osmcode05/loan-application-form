ba1f8857d7dd941ba240b32b545d25491e19693f
oussama ait moummad <ousamatessla@gmail.com>
2024-08-16 16:36:44 +0100

    Change background color

--- src/App.jsx
+++ src/App.jsx
@@ -14,95 +14,105 @@ export default function App() {
   let [showAlert, setShowAlert] = useState(false);
 
   return (
-    <section>
-      <h1>Requesting a Loan</h1>
-      <hr />
-      <form>
-        <label>Full Name : </label>
-        <input
-          type="text"
-          value={UserData.name}
-          onChange={(e) => setUserData({ ...UserData, name: e.target.value })}
-        />
-      </form>
+    <main className="main">
+      <section>
+        <h1>Requesting a Loan</h1>
+        <hr />
+        <form>
+          <label>Full Name : </label>
+          <input
+            type="text"
+            value={UserData.name}
+            onChange={(e) => setUserData({ ...UserData, name: e.target.value })}
+          />
+        </form>
 
-      <form>
-        <label>Phone Number: </label>
-        <input
-          type="number"
-          value={UserData.phoneNumber}
-          onChange={(e) =>
-            setUserData({ ...UserData, phoneNumber: e.target.value })
-          }
-        />
-      </form>
+        <form>
+          <label>Phone Number: </label>
+          <input
+            type="number"
+            value={UserData.phoneNumber}
+            onChange={(e) =>
+              setUserData({ ...UserData, phoneNumber: e.target.value })
+            }
+          />
+        </form>
 
-      <form>
-        <label>Age: </label>
-        <input
-          type="number"
-          value={UserData.Age}
-          onChange={(e) => setUserData({ ...UserData, Age: e.target.value })}
-        />
-      </form>
+        <form>
+          <label>Age: </label>
+          <input
+            type="number"
+            value={UserData.Age}
+            onChange={(e) => setUserData({ ...UserData, Age: e.target.value })}
+          />
+        </form>
 
-      <h3 style={{ color: "#fff" }}>Are you an employee ?</h3>
-      <form>
-        <label>Yes</label>
-        <input
-          type="radio"
-          name="question"
-          value="Yes"
-          checked={UserData.isEmployee === "Yes"}
-          onChange={(e) => {
-            setUserData({ ...UserData, isEmployee: e.target.value });
+        <h3 style={{ color: "#fff" }}>Are you an employee ?</h3>
+        <form>
+          <label>Yes</label>
+          <input
+            type="radio"
+            name="question"
+            value="Yes"
+            checked={UserData.isEmployee === "Yes"}
+            onChange={(e) => {
+              setUserData({ ...UserData, isEmployee: e.target.value });
+            }}
+          />
+          <label>No</label>
+          <input
+            type="radio"
+            name="question"
+            value="No"
+            checked={UserData.isEmployee === "No"}
+            onChange={(e) => {
+              setUserData({ ...UserData, isEmployee: e.target.value });
+            }}
+          />
+        </form>
+
+        <h3 style={{ color: "#fff" }}>Salary</h3>
+        <select
+          style={{ padding: "4px" }}
+          value={UserData.salary}
+          onChange={(e) => setUserData({ ...UserData, salary: e.target.value })}
+        >
+          <option value="less than 500$">less than 500$</option>
+          <option value="between 500$ and 1000$">between 500$ and 1000$</option>
+          <option value="above 1000$">above 1000$</option>
+        </select>
+
+        <button
+          type="submit"
+          style={{
+            backgroundColor:
+              UserData.name !== "" &&
+              UserData.phoneNumber !== "" &&
+              UserData.Age !== ""
+                ? "#fb0fbc"
+                : "#acacac",
+            pointerEvents:
+              UserData.name !== "" &&
+              UserData.phoneNumber !== "" &&
+              UserData.Age !== ""
+                ? "auto"
+                : "none",
           }}
-        />
-        <label>No</label>
-        <input
-          type="radio"
-          name="question"
-          value="No"
-          checked={UserData.isEmployee === "No"}
-          onChange={(e) => {
-            setUserData({ ...UserData, isEmployee: e.target.value });
+          onClick={() => setShowAlert(true)}
+          onBlur={() => {
+            setShowAlert(false);
+            setUserData({
+              ...UserData,
+              name: "",
+              phoneNumber: "",
+              Age: "",
+            });
           }}
-        />
-      </form>
-
-      <h3 style={{ color: "#fff" }}>Salary</h3>
-      <select
-        style={{ padding: "4px" }}
-        value={UserData.salary}
-        onChange={(e) => setUserData({ ...UserData, salary: e.target.value })}
-      >
-        <option value="less than 500$">less than 500$</option>
-        <option value="between 500$ and 1000$">between 500$ and 1000$</option>
-        <option value="above 1000$">above 1000$</option>
-      </select>
-
-      <button
-        type="submit"
-        style={{
-          backgroundColor:
-            UserData.name !== "" &&
-            UserData.phoneNumber !== "" &&
-            UserData.Age !== ""
-              ? "#fb0fbc"
-              : "#acacac",
-          pointerEvents:
-            UserData.name !== "" &&
-            UserData.phoneNumber !== "" &&
-            UserData.Age !== ""
-              ? "auto"
-              : "none",
-        }}
-        onClick={() => setShowAlert(true)}
-        onBlur={() => setShowAlert(false)}
-      >
-        Submit
-      </button>
-      {showAlert && <AlertCmp UserData={UserData} />}
-    </section>
+        >
+          Submit
+        </button>
+        {showAlert && <AlertCmp UserData={UserData} />}
+      </section>
+    </main>
   );
 }
